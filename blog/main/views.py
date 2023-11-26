4deeae36d2846c9fb645ce3adc5f830f69c9ff16
exemayj <maximserebro@gmail.com>
2023-11-26 21:54:16 +0300

    create on main info about weather

--- blog/main/views.py
+++ blog/main/views.py
@@ -1,14 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
+from pyowm import OWM
+from pyowm.utils import config
+from pyowm.utils import timestamps
 
 def index(request):
-    data = {
-        'title':'Main page',
+
+    context = {
+        'current_temp': get_weather()
     }
-    return render(request, 'main/index.html',)
+    
+    return render(request, 'main/index.html', context)
+
+
+
+def get_weather():
+    owm = OWM('39adf2a129e3475f7055c32a84a08296')
+    mgr = owm.weather_manager()
+
+
+    # Search for current weather in London (Great Britain) and get details
+    observation = mgr.weather_at_place('Saint Petersburg')
+    w = observation.weather
+
+    w.detailed_status         # 'clouds'
+    t = w.temperature('celsius')['temp']  # {'temp_max': 10.5, 'temp': 9.7, 'temp_min': 9.0}
+    return round(t)
+
+
+
 
 def about(request):
     return render(request, 'main/about.html')
-
-def profile(request):
-    return render(request, 'main/profile.html')
